e66a7fd332e39c167f4f1f343d2215ab10e4f571
Om Thakkar <om.thakkar@intel.com>
2026-04-29 06:27:25 -0700

    decoder: bug fix

--- src/decoder.py
+++ src/decoder.py
@@ -7,7 +7,7 @@ from ffn import FFN
 
 class DecoderBlock:
   def __init__(self, D_model, D_k, D_v, H, key=jax.random.PRNGKey(0)):
-    keys = jax.random.split(key, 3)
+    keys = jax.random.split(key, 2)
     self.attn = MultiHeadAttention(D_model, D_k, D_v, H, key=keys[0])
     self.ln1 = LayerNorm(D_model)
     self.ln2 = LayerNorm(D_model)
